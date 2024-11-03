703d653c59ae12fa382baa4d981eba7d4315c356
MOKASHI Zahirkhan (CONTRACTOR) <zaheerk0302@gmail.com>
2024-11-03 17:56:01 +0530

    updated

--- src/main/java/com/zaheer/movies/MovieController.java
+++ src/main/java/com/zaheer/movies/MovieController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+
+@CrossOrigin(origins = "https://movieverse-git-master-bugger0s-projects.vercel.app/")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
